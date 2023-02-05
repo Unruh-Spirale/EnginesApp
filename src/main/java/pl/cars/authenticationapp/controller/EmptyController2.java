269eafce99cb980388a01acd5aa6ed20294e8008
Unruh-Spirale <protjan@wp.pl>
2023-02-06 00:32:08 +0100

    Add second empty controller

---
+++ src/main/java/pl/cars/authenticationapp/controller/EmptyController2.java
@@ -0,0 +1,11 @@
+package pl.cars.authenticationapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmptyController2 {
+
+    public void emptyHelloWorld(){
+        System.out.println("Hello World2!");
+    }
+}
