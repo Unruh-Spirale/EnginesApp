729b5fa8f2388da1772b432718cf875dc9098f13
Unruh-Spirale <protjan@wp.pl>
2023-02-06 00:32:41 +0100

    Fix second empty controller

--- src/main/java/pl/cars/authenticationapp/controller/EmptyController2.java
+++ src/main/java/pl/cars/authenticationapp/controller/EmptyController2.java
@@ -1,10 +1,12 @@
 package pl.cars.authenticationapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmptyController2 {
 
+    @GetMapping("/helloWorld2")
     public void emptyHelloWorld(){
         System.out.println("Hello World2!");
     }
